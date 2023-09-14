54b7ff294df89eb34aa310063c0be36ef102cc61
Thomas Nguyen <tomxnguy@gmail.com>
2023-09-14 18:35:54 +0000

    Completed dom-event-delegation exercise.

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,12 @@
+const $taskList = document.querySelector('.task-list');
+$taskList.addEventListener('click', handleTaskList);
+
+function handleTaskList(event) {
+  console.log('event.target', event.target);
+  console.log('event.target.tagName', event.target.tagName);
+  if (event.target && event.target.tagName === 'BUTTON') {
+    const closestItem = event.target.closest('.task-list-item');
+    console.log('.task.list.item', closestItem);
+    closestItem.remove(closestItem);
+  }
+}
