eba5ea5b6880dde34c458895270dbb8b7e8c53d5
Thomas Nguyen <tomxnguy@gmail.com>
2023-09-15 19:03:30 +0000

    Completed javascript-and-json exercise.

---
+++ javascript-and-json/main.js
@@ -0,0 +1,32 @@
+const arr = [
+  {
+    isbn: '9780375828379',
+    title: 'Hop On Pop',
+    author: 'Dr.Seuss',
+  },
+  {
+    isbn: '0679735771',
+    title: 'American Psycho',
+    author: 'Bret Easton Ellis',
+  },
+  {
+    isbn: '0393355942',
+    title: 'Fight Club: A Novel',
+    author: 'Chuck Palahniuk',
+  },
+];
+
+console.log('array typeof: ', typeof arr);
+
+const stringify = JSON.stringify(arr);
+console.log(stringify);
+console.log('JSON type of: ', typeof stringify);
+
+const newString =
+  '[{"Number id": "12345", "name":"Thomas"}, {"Number id":"56789", "name":"Shawn"}]';
+console.log('newString: ', newString);
+console.log('newString typeof: ', typeof newString);
+
+const json = JSON.parse(newString);
+console.log('json: ', json);
+console.log('json typeof: ', typeof json);
