ec902d89b8b9d7ce788c78351e3b49de7b24a73b
Thomas Nguyen <tomxnguy@gmail.com>
2023-10-30 16:00:28 +0000

    react rendering lists save

---
+++ react-rendering-lists/render-list/src/PokemonList.tsx
@@ -0,0 +1,16 @@
+// export type Pokemon = {
+//   number:string;
+//   name:string;
+// }
+
+// type Props = {
+//   pokedex:Pokemon[];
+// }
+
+// export default function PokemonList({pokedex}:Props){
+//   return (
+//   <ul>
+//     const pokemon = pokedex.map(pokes => <li key={pokemon.number}> {pokemon.name}) </li>
+//   </ul>
+//   )
+// }
