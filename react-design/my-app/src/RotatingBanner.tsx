4a0d7779312b0ddc36a1b7fb83627c1f9cc00c08
Thomas Nguyen <tomxnguy@gmail.com>
2023-10-31 18:30:46 +0000

    react-design save.

---
+++ react-design/my-app/src/RotatingBanner.tsx
@@ -0,0 +1,41 @@
+// type Props = {
+//   items: string[];
+// };
+
+// export default function RotatingBanner({ items }: Props) {
+//   return (
+//     <>
+//       <Banner item={items[0]} />
+//       <Button text="Prev" />
+//       <Indicators count={items.length} />
+//       <Button text="Next" />
+//     </>
+//   );
+// }
+
+// type BannerProps = {
+//   item: string;
+// };
+
+// function Banner({ item }: BannerProps) {
+//   return <h1>{item}</h1>;
+// }
+
+// type ButtonProps = {
+//   text: string;
+// };
+
+// function Button({ text }: ButtonProps) {
+//   return <button>{text}</button>;
+// }
+
+// type IndicatorProps = {
+//   count: string;
+// };
+
+// function Indicators({ count }: IndicatorProps) {
+//   for (let i = 0; i < items.length; i++) {
+//     return i;
+//   }
+//   return <div>{buttons}</div>;
+// }
