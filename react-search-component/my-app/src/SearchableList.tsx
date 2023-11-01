d996fe19e46995fa9bfc848c1b77b28b98bc9275
Thomas Nguyen <tomxnguy@gmail.com>
2023-11-01 13:58:19 +0000

    Completed react-search-component exercise.

--- react-search-component/my-app/src/SearchableList.tsx
+++ react-search-component/my-app/src/SearchableList.tsx
@@ -1,55 +1,52 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
-// export default function SearchableList() {
-//   return (
-//     <>
-//       <SearchBar />
-//       <QuoteList />
-//     </>
-//   );
-// }
+type Props = {
+  list: string[];
+};
 
-// function SearchBar() {
-//   const [search, setSearch] = useState('');
-//   const [filteredQuotes, setFilteredQuotes] = useState(quotes);
+export default function SearchableList({ list }: Props) {
+  const [value, setValue] = useState('');
+  const filtered = list.filter((item) =>
+    item.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+  );
 
-//   const handleInput = (e) => {
-//     const searchTerm = e.target.value;
-//     setSearch(searchTerm);
+  return (
+    <div className="wrapper">
+      <SearchBar value={value} onChange={setValue} />
+      <Items items={filtered} />
+    </div>
+  );
+}
 
-//     const filteredItems = quotes.filter((quote) =>
-//       quote.toLowerCase().includes(searchTerm.toLowerCase())
-//     );
+type SearchBarProps = {
+  value: string;
+  onChange: (value: string) => void;
+};
 
-//     setFilteredQuotes(filteredItems);
-//   };
+function SearchBar({ value, onChange }: SearchBarProps) {
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={(e) => onChange(e.currentTarget.value)}
+      className="search"
+    />
+  );
+}
 
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         placeholder="search"
-//         value={search}
-//         onChange={handleInput}
-//       />
-//     </div>
-//   );
-// }
+type ItemsProps = {
+  items: Props['list'];
+};
 
-// function QuoteList() {
-//   const quotes = [
-//     'Yer a wizard Harry',
-//     "I hope you're pleased with yourselves",
-//     'It does not do well to dwell on dreams and forget to live',
-//     'To the well-organized mind, death is but the next great adventure.',
-//     'Youre a little scary sometimes, you know that? Brilliant... but scary',
-//     'There will be no foolish wand waving or silly incantations in this class.',
-//     'It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends.',
-//     'If there is one thing Voldemort cannot understand, it is love.',
-//     'As much money and life as you could want!',
-//   ];
-
-//   const quoteItems = quotes.map((quote) => <li>{quote}</li>);
-
-//   return <ul>{quoteItems}</ul>;
-// }
+function Items({ items }: ItemsProps) {
+  if (items.length === 0) {
+    return <div>Nothing found!</div>;
+  }
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+}
